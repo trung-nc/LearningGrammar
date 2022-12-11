91f66b74f0f7b8692dd84b02338dab5b658bef14
trung-nc <trungnguyenitk65@gmail.com>
2022-12-12 03:21:32 +0700

    ui_v1

---
+++ App.js
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import Home from "./screens/home"
+import Search from "./screens/search"
+import Profile from "./screens/profile"
+import SignInScreen from "./screens/login"
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={{ headerShown: false, }}
+      tabBarOption={{
+        style: {
+          backgroundColor: 'blue',
+        }
+      }}
+    >
+      <Tab.Screen name="Login" component={SignInScreen} 
+        options={{
+          tabBarIcon: ({color, focused}) => (
+            <Icon name="at" size={26} color="#52575D"></Icon>
+          )
+        }}
+      />
+
+      <Tab.Screen name="Home" component={Home} 
+        options={{
+          tabBarIcon: ({color, focused}) => (
+            <Icon name="home" size={26} color="#52575D"></Icon>
+          )
+        }}
+      />
+
+      <Tab.Screen name="Search" component={Search} 
+        options={{
+          tabBarIcon: ({color, focused}) => (
+            <Icon name="search" size={26} color="#52575D"></Icon>
+          )
+        }}
+      />
+
+      <Tab.Screen name="Profile" component={Profile} 
+        options={{
+          tabBarIcon: ({color, focused}) => (
+            <Icon name="user" size={26} color="#52575D"></Icon>
+          )
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+const App = () => {
+  return (
+  <NavigationContainer>
+    <MyTabs />
+  </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ screens/profile.js
@@ -0,0 +1,100 @@
+import React from "react";
+import { StyleSheet, Text, View, Image, ScrollView, useWindowDimensions } from "react-native";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import CustomButton from '../components/CustomButton';
+
+const Profile = () => {
+  const { height } = useWindowDimensions();
+
+  const onSavedCourse = () => {
+    console.warn("onSavedCourse");
+  }
+
+  const onAchievements = () => {
+    console.warn("onAchievements");
+  }
+
+  const onAboutUs = () => {
+    console.warn("onAboutUs");
+  }
+
+  const onSignOut = () => {
+    console.warn("onSignOut");
+  }
+
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.root}>
+        <View style={[styles.profileImage, {marginTop: 20}]}>
+          <Image 
+            source={require("../Image/avatar.png")}
+            style={[styles.logo, { height: height * 0.3}]}
+            resizeMode="contain"
+          />
+        </View>
+      
+        <View style={styles.infoContainer}>
+          <Text style={{ color: "black", fontWeight: "200", fontSize: 36 }}>Johny English</Text>
+          <Text style={{ color: "black", fontSize: 14, alignSelf: "center" }}>username</Text>
+        </View>
+        
+
+        <View style={[styles.infoContainer, {marginTop: 22}]}>
+          <Text style={{ color: "black", fontSize: 14, fontWeight: "bold", }}>GENERAL</Text>
+        </View>
+
+        <CustomButton 
+          text="Saved courses"
+          onPress={onSavedCourse}
+          // type="TERTIARY"
+        />
+
+        <CustomButton 
+          text="My Achivements"
+          onPress={onAchievements}
+          // type="TERTIARY"
+        />
+
+        <View style={styles.infoContainer}>
+          <Text style={{ color: "black", fontSize: 14, fontWeight: "bold", }}>SETTING</Text>
+        </View>
+
+        <CustomButton 
+          text="About us"
+          onPress={onAboutUs}
+          // type="TERTIARY"
+        />
+
+        <CustomButton 
+          text="Sign out"
+          onPress={onSignOut}
+          // type="TERTIARY"
+        />
+
+
+        
+      </View>
+    </ScrollView>
+  );
+}
+
+export default Profile;
+
+const styles = StyleSheet.create({
+  root: {
+    alignItems: 'center',
+    padding: 20,
+  },
+  profileImage: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden"
+  },
+  infoContainer: {
+    alignSelf: "center",
+    marginTop: 16
+  },
+});
